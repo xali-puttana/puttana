8962c7260d1371998535539f891ed5efbe8b79d5
xali-puttana <xxali1201@gmail.com>
2024-09-23 14:17:03 +0800

    Delete example_dynamic_class.lua

--- example_dynamic_class.lua
+++
@@ -1,8 +0,0 @@
-
-local function loadClass_(param) 
-  local args = param.args;
-  local name = tostring(args[0]);
-  dbgview("ClassLoader.loadClass: " .. name);
-end
-
-hook("java/lang/ClassLoader", "loadClass", "Ljava/lang/String;", loadClass_, nil);
