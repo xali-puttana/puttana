a5ec3a8a5ce9073da60ccbc4063da4549a63d081
xali-puttana <xxali1201@gmail.com>
2024-09-23 14:17:12 +0800

    Delete example_file.lua

--- example_file.lua
+++
@@ -1,9 +0,0 @@
-
-hook("java/io/File", "delete", "*", nil, function (param)
-    local file = param.thisObject;
-    reflect_assertObject(file);
-    local path = file:getPath();
-    reflect_assertObject(path);
-    dbgview("File.delete: " .. path);
-end
-);
