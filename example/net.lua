a0d47e4f2ce88ba18539a2f180eae5ab9cb157e0
xali-puttana <xxali1201@gmail.com>
2024-09-26 13:43:13 +0800

    Update net.lua

--- example/net.lua
+++ example/net.lua
@@ -8,3 +8,23 @@ hook("java/net/URL", "openConnection", "*", nil,
     dbgview("openConnection: " .. val);
   end
 );
+
+function hookOutputStream(param)
+  local stream = param:getResult();
+  if reflect_checkObject(stream) then
+    local OutputStreamClass = reflect_findClass("java/io/OutputStream");
+    local proxy = proxyObject(
+      function (param0)
+        if "public void java.io.OutputStream.write(byte[]) throws java.io.IOException" == tostring(param0.method) then
+            local args = param0.args;
+            local String = reflect_findClass("java/lang/String");
+            dbgview("HttpsURLConnectionImpl.write: " .. String(args[0], "utf8"));
+        end
+      end, nil, 
+      stream, OutputStreamClass
+    );
+    param:setResult(proxy);
+  end  
+end
+local class = reflect_findClass("com/android/okhttp/internal/huc/HttpsURLConnectionImpl");
+hook(class, "getOutputStream", "*", nil, hookOutputStream);
